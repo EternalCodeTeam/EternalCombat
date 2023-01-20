68e66b59cb1388511fe75cbc1385deeac9f26aa8
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2023-01-20 17:53:29 +0100

    Update src/main/java/com/eternalcode/combatlog/config/implementation/MessageConfig.java
    Co-authored-by: Jakub Kędziora <77227023+Jakubk15@users.noreply.github.com>

--- src/main/java/com/eternalcode/combatlog/config/implementation/MessageConfig.java
+++ src/main/java/com/eternalcode/combatlog/config/implementation/MessageConfig.java
@@ -16,7 +16,7 @@ public class MessageConfig implements ReloadableConfig {
     public String tagPlayer = "&cYou are in combat, do not leave the server!";
     public String unTagPlayer = "&aYou are no longer in combat! You can safely leave the server.";
     public String cantUseCommand = "&cUsing this command during combat is prohibited!";
-    public String adminTagPlayer = "&7You have given combatLog to &e{FIRST_PLAYER}&7 and &e{SECOND_PLAYER}&7.";
+    public String adminTagPlayer = "&7You have tagged &e{FIRST_PLAYER}&7 and &e{SECOND_PLAYER}&7.";
     public String adminUnTagPlayer = "&7You have removed &e{PLAYER} from fight.";
     public String invalidUsage = "&7Correct usage: &e{COMMAND}.";
     public String inventoryBlocked = "&cYou cannot open this inventory during combat!";
