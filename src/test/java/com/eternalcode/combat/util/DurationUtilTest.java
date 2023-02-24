765ca2f6086296741f4a479e61ec3f25730119fa
Alex Noyle <alexnoyle@icloud.com>
2023-02-24 13:51:46 +0000

    Fix gradle build (#44)

--- src/test/java/com/eternalcode/combat/util/DurationUtilTest.java
+++ src/test/java/com/eternalcode/combat/util/DurationUtilTest.java
@@ -1,13 +1,13 @@
 package com.eternalcode.combat.util;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Collection;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class DurationUtilTest {
 
