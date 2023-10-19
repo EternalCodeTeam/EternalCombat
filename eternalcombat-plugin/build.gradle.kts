88a85184817e75787a11edf23575970c68c4154f
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2023-10-19 20:55:44 +0200

    Centralized Dependency and Project Configuration Management.

--- eternalcombat-plugin/build.gradle.kts
+++ eternalcombat-plugin/build.gradle.kts
@@ -1,32 +1,7 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-
 plugins {
-    `java-library`
-    checkstyle
-
-    id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("xyz.jpenilla.run-paper") version "2.2.0"
-}
-
-checkstyle {
-    toolVersion = "10.12.4"
-
-    configFile = file("${rootDir}/config/checkstyle/checkstyle.xml")
-
-    maxErrors = 0
-    maxWarnings = 0
-}
-
-repositories {
-    mavenCentral()
-
-    maven { url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") }
-    maven { url = uri("https://papermc.io/repo/repository/maven-public/") }
-    maven { url = uri("https://repo.eternalcode.pl/releases") }
-    maven { url = uri("https://storehouse.okaeri.eu/repository/maven-public/") }
-    maven { url = uri("https://repo.panda-lang.org/releases") }
-    maven { url = uri("https://maven.enginehub.org/repo/") }
+    alias(libs.plugins.pluginyml)
+    alias(libs.plugins.shadow)
+    alias(libs.plugins.runpaper)
 }
 
 dependencies {
@@ -43,23 +18,13 @@ bukkit {
     version = "${project.version}"
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
-
-tasks.withType<JavaCompile> {
-    options.compilerArgs = listOf("-Xlint:deprecation", "-parameters")
-    options.encoding = "UTF-8"
-}
-
 tasks {
     runServer {
         minecraftVersion("1.19.4")
     }
 }
 
-tasks.withType<ShadowJar> {
+tasks.shadowJar {
     archiveFileName.set("EternalCombat v${project.version}.jar")
 
     dependsOn("checkstyleMain")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,8 @@
+/*
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+*/
+
 rootProject.name = "EternalCombat"
 include("eternalcombat-api")
 include("eternalcombat-plugin")
+
