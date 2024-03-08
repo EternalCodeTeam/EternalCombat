94113ea32663297b688c36d61fc884ff9f0317f6
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2024-03-08 23:37:05 +0100

    Fix checkstyle.

--- buildSrc/src/main/kotlin/eternalcombat.java.gradle.kts
+++ buildSrc/src/main/kotlin/eternalcombat.java.gradle.kts
@@ -15,6 +15,17 @@ checkstyle {
     maxWarnings = 0
 }
 
+// https://github.com/JabRef/jabref/pull/10812/files#diff-49a96e7eea8a94af862798a45174e6ac43eb4f8b4bd40759b5da63ba31ec3ef7R267
+configurations.named("checkstyle") {
+    resolutionStrategy {
+        capabilitiesResolution {
+            withCapability("com.google.collections:google-collections") {
+                select("com.google.guava:guava:33.0.0-jre")
+            }
+        }
+    }
+}
+
 repositories {
     mavenCentral()
 
