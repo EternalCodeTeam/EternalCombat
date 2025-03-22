7f130493f7effb1209c66c29249c4041ac61de9b
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2025-03-22 02:41:34 +0100

    Fix java settings.

--- buildSrc/src/main/kotlin/eternalcombat-java.gradle.kts
+++ buildSrc/src/main/kotlin/eternalcombat-java.gradle.kts
@@ -5,11 +5,6 @@ plugins {
 group = "com.eternalcode"
 version = "2.0.0-SNAPSHOT"
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
-
 tasks.compileJava {
     options.compilerArgs = listOf("-Xlint:deprecation", "-parameters")
     options.encoding = "UTF-8"
