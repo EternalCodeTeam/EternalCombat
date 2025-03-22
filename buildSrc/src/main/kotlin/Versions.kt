c55d95e24dcbf7d5b390dcc6c155f1c4a420bb4f
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2025-03-22 22:16:05 +0100

    Keep worldguard version.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -27,7 +27,7 @@ object Versions {
     const val CAFFEINE = "3.2.0"
 
     const val B_STATS_BUKKIT = "3.1.0"
-    const val WORLD_GUARD_BUKKIT = "7.0.13"
+    const val WORLD_GUARD_BUKKIT = "7.0.9"
 
     const val PLACEHOLDER_API = "2.11.6"
     const val PAPERLIB = "1.0.8"
