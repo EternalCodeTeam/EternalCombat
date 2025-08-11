eb22799c2e490e7f23023ffc4901cde132727c96
Piotr Zych <77621271+P1otrulla@users.noreply.github.com>
2025-08-11 19:33:14 +0200

    rollback version

--- eternalcombat-plugin/build.gradle.kts
+++ eternalcombat-plugin/build.gradle.kts
@@ -74,7 +74,7 @@ bukkit {
 
 tasks {
     runServer {
-        minecraftVersion("1.21")
+        minecraftVersion("1.21.4")
         downloadPlugins.url("https://github.com/retrooper/packetevents/releases/download/v2.8.0/packetevents-spigot-2.8.0.jar")
     }
 }
