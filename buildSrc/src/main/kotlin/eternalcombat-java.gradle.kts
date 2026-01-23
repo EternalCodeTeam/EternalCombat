0979d449c657ae5e017e7034abe902fcbcd19c24
Martin Sulikowski <vluckyyy.biznes@gmail.com>
2026-01-23 20:37:50 +0100

    Release 2.4.0

--- buildSrc/src/main/kotlin/eternalcombat-java.gradle.kts
+++ buildSrc/src/main/kotlin/eternalcombat-java.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "com.eternalcode"
-version = "2.4.0-SNAPSHOT"
+version = "2.4.0"
 
 tasks.compileJava {
     options.compilerArgs = listOf("-Xlint:deprecation", "-parameters")
--- buildSrc/src/main/kotlin/eternalcombat-publish.gradle.kts
+++ buildSrc/src/main/kotlin/eternalcombat-publish.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.eternalcode"
-version = "2.4.0-SNAPSHOT"
+version = "2.4.0"
 
 java {
     withSourcesJar()
