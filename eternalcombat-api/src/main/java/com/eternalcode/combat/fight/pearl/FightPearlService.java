ba4866fb4667994db9fbe78f070b1cea0870b0e6
Rollczi <ndejlich5@gmail.com>
2026-02-07 22:57:11 +0100

    CR

--- eternalcombat-api/src/main/java/com/eternalcode/combat/fight/pearl/FightPearlService.java
+++ eternalcombat-api/src/main/java/com/eternalcode/combat/fight/pearl/FightPearlService.java
@@ -1,12 +1,9 @@
 package com.eternalcode.combat.fight.pearl;
 
 import java.time.Duration;
-import java.time.Instant;
 import java.util.UUID;
 
 public interface FightPearlService {
-
-    Instant getDelay(UUID uuid);
 
     Duration getRemainingDelay(UUID uuid);
 
