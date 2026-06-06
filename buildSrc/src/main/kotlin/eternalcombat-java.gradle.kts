74ec79558160e97c3b7f5b11894595431bbdf64a
Martin Sulikowski <vluckyyy.biznes@gmail.com>
2026-06-06 02:03:30 +0200

    Fix: bump version to 2.5.1-SNAPSHOT for further development

--- buildSrc/src/main/kotlin/eternalcombat-java.gradle.kts
+++ buildSrc/src/main/kotlin/eternalcombat-java.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "com.eternalcode"
-version = "2.5.0-SNAPSHOT"
+version = "2.5.1-SNAPSHOT"
 
 tasks.compileJava {
     options.compilerArgs = listOf("-Xlint:deprecation", "-parameters")
