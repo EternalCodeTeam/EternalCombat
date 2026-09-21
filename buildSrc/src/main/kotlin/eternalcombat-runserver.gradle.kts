2265b66dfc6b09356f3d23bdd5343fe50cdf7fa1
Martin Sulikowski <vluckyyy.biznes@gmail.com>
2026-09-22 00:38:38 +0200

    Update dependencies for Minecraft 26.2 compatibility and improve README

--- buildSrc/src/main/kotlin/eternalcombat-runserver.gradle.kts
+++ buildSrc/src/main/kotlin/eternalcombat-runserver.gradle.kts
@@ -13,10 +13,10 @@ tasks {
             languageVersion.set(JavaLanguageVersion.of(25))
         })
 
-        minecraftVersion("26.1.2")
-        downloadPlugins.modrinth("FastAsyncWorldEdit", "2.15.2")
-        downloadPlugins.modrinth("PacketEvents", "2.12.2+spigot")
-        downloadPlugins.modrinth("WorldGuard", "7.0.17")
+        minecraftVersion("26.2")
+        downloadPlugins.modrinth("WorldEdit", "7.4.6-beta-01")
+        downloadPlugins.modrinth("PacketEvents", "2.13.0+spigot")
+        downloadPlugins.modrinth("WorldGuard", "7.0.19")
         downloadPlugins.modrinth("LuckPerms", "v5.5.53-bukkit")
         downloadPlugins.modrinth("GriefPrevention", Versions.GRIEF_PREVENTION)
     }
